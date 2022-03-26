605bb789fa584277a0c59d44176f171dab038e32
Raneemoqaily7 <raneemoqaily7@gmail.com>
2022-03-26 23:39:19 +0300

    stack-and-queue

--- linked-list-kth/linked_list_kth.py
+++ linked-list-kth/linked_list_kth.py
@@ -1,40 +1,103 @@
+
 class Node :                                                     
-    def __init__(self,value,next):
+    def __init__(self,value=None,next=None):
         self.value=value
-        self.next=next
+        self.next= next
 
                              
 class LinkedList:
     def __init__(self):
         self.head=None
+        
 
+        
     def push(self,new_value):
         new_node=Node(new_value)
-        new_node.next=self.head
-        self.head=new_node
-
-
-
-    def getKthNode(self,head,position):
-        count=0
-        if (head ):
-            if count == position:
-                print (head.value)
-
-            else:
-                list.getKthNode(head.next, position-1)
+        if self.head is None:
+            
+            self.head=new_node
+       
         else :
-            print("index doesnt exist")
+           
+            current=self.head
+            while current.next is not None :
+                current=current.next
+            current.next =new_node
 
 
+    def __str__(self):
+        if self.head is None :
+            return 'Empty Linked List'
+
+        else :
+            current =self.head
+            output =""
+            while current is not None :
+                output+=f'{current.value} -->'
+                
+                current =current.next
+            output += "NULL"
+            return output
+                
+    
+
+    def get_length(self):
+        count =0
+        current =self.head
+        while current is  not  None :
+            
+            current=current.next
+            count+=1
+        return count
+
+#[1,2,3,4] 
+    def reverse(self):
+        
+
+      prev=None
+      current =self.head
+      while current is not None :
+          next =current.next
+          current.next=prev
+          prev=current
+          current =next
+      self.head =prev
+
+
+
+                     
+            
+
+        
+    
+
+#[1,2,3,4,5]
+    # def getKthNode(self,head,position):
+    #     count=0
+    #     if (head ):
+    #         if count == position:
+    #             print (head.value)
+
+    #         else:
+    #             list.getKthNode(head.next, position-1)
+
+    #     else :
+    #         print("index doesnt exist")
+
+    
 if __name__ == "__main__":
     list=LinkedList()
+    
     list.push(1)
     list.push(2)
     list.push(3)
     list.push(4)
-    list.push(5)
-    list.getKthNode(list,3)
+    
+    print(list)
+
+    print(list.reverse()) 
+    print(list)
+
 
 
   
