16e56f97b128e816730d97e515adea3271767345
Raneemoqaily7 <raneemoqaily7@gmail.com>
2022-04-15 14:31:53 +0300

    tests

--- trees/tests/test_trees.py
+++ trees/tests/test_trees.py
@@ -1,24 +1,190 @@
-from trees.trees import Binary_Tree ,build_tree
+from trees.trees import Node ,BinaryTree,BinarySearchTree
 
-def test_in_order():
-    numbers = [2,4,5,1,2,8,345,2,6]
-    numbers_tree = build_tree(numbers)
-    actual = numbers_tree.in_order()
-    expected = [1, 2, 4, 5, 6, 8, 345]
+def test_pre_order_one():
+    node1=Node(1)
+    node2=Node(2)
+    node1.left=node2
+    
+    node3=Node(3)
+    node1.right=node3
+    node4=Node(4)
+    node2.left=node4
+    
+    tree = BinaryTree()
+    tree.root=node1
+    actual = tree.pre_order()
+    expected =[1, 2, 4, 3]
+    assert actual == expected
+
+def test_in_order_two():
+    node1=Node(1)
+    node2=Node(2)
+    node1.left=node2
+    
+    node3=Node(3)
+    node1.right=node3
+    node4=Node(4)
+    node2.left=node4
+    
+    tree = BinaryTree()
+    tree.root=node1
+    actual = tree.in_order()
+    expected =[4, 2, 1, 3]
+    assert actual == expected
+
+def test_post_order_three():
+    node1=Node(1)
+    node2=Node(2)
+    node1.left=node2
+    
+    node3=Node(3)
+    node1.right=node3
+    node4=Node(4)
+    node2.left=node4
+    
+    tree = BinaryTree()
+    tree.root=node1
+    actual = tree.post_order()
+    expected =[4, 2, 3, 1]
     assert actual == expected
 
 
-def test_pre_order():
-    numbers = [2,4,5,1,2,8,345,2,6]
-    numbers_tree = build_tree(numbers)
-    actual = numbers_tree.pre_order()
-    expected = [2, 1, 4, 5, 6, 8, 345]
+def test_single_root_node():
+    node1=Node(1)
+    tree = BinaryTree()
+    tree.root=node1
+    actual = tree.post_order()
+    expected =[1]
     assert actual == expected
 
-def test_post_order():
-    numbers = [2,4,5,1,2,8,345,2,6]
-    numbers_tree = build_tree(numbers)
-    actual = numbers_tree.post_order()
-    expected = [1, 4, 5, 6, 8, 345, 2]
+def test_add_value_to_the_right():
+    node1=Node(4)
+    tree1 = BinarySearchTree()
+    tree1.root = node1 
+    
+    node2=Node(2)
+    node3 =Node(8)
+    node4=Node(9)
+    
+    node1.left=node2
+    node1.right=node3
+    node3.right=node4
+    tree1.Add(9)
+    actual =tree1.pre_order()
+     
+    expected = [4, 2, 8, 9]
     assert actual == expected
+
+def test_add_value_to_the_left():
+    node1=Node(4)
+    tree1 = BinarySearchTree()
+    tree1.root = node1 
+    
+    node2=Node(2)
+    node3 =Node(8)
+    
+    
+    node1.left=node2
+    node1.right=node3
+    
+    tree1.Add(1)
+    actual =tree1.pre_order()
+     
+    expected = [4, 2, 1, 8]
+    assert actual == expected
+
+def test_collection_from_pre_order_traversal():
+    tree1 = BinarySearchTree()
+    node1=Node(4)
+    tree1.root = node1 
+    
+    node2=Node(2)
+    node3 =Node(8)
+    node4=Node(9)
+    node1.left=node2
+    node1.right=node3
+    node3.right=node4
+    tree1.Add(9)
+
+    actual =tree1.pre_order()
+    expected =[4, 2, 8, 9]
+    assert actual == expected
+
+def test_collection_from_in_order_traversal():
+    tree1 = BinarySearchTree()
+    node1=Node(4)
+    tree1.root = node1 
+    
+    node2=Node(2)
+    node3 =Node(8)
+    node4=Node(9)
+    node1.left=node2
+    node1.right=node3
+    node3.right=node4
+    tree1.Add(9)
+
+    actual =tree1.in_order()
+    expected =[2, 4, 8, 9]
+    assert actual == expected
+
+def test_collection_from_post_order_traversal():
+    tree1 = BinarySearchTree()
+    node1=Node(4)
+    tree1.root = node1 
+    
+    node2=Node(2)
+    node3 =Node(8)
+    node4=Node(9)
+    node1.left=node2
+    node1.right=node3
+    node3.right=node4
+    tree1.Add(9)
+
+    actual =tree1.post_order()
+    expected =[2, 9, 8, 4]
+    assert actual == expected
+
+
+
+def test_Contains_existing_value():
+
+    tree1 = BinarySearchTree()
+    node1=Node(4)
+    tree1.root = node1 
+    
+    node2=Node(2)
+    node3 =Node(8)
+    node4=Node(9)
+    node1.left=node2
+    node1.right=node3
+    node3.right=node4
+    tree1.Contains(9)
+
+    actual =tree1.Contains(9)
+    expected =True
+    assert actual == expected
+
+def test_Contains_not_existing_value():
+
+    tree1 = BinarySearchTree()
+    node1=Node(4)
+    tree1.root = node1 
+    
+    node2=Node(2)
+    node3 =Node(8)
+    node4=Node(9)
+    node1.left=node2
+    node1.right=node3
+    node3.right=node4
+    tree1.Contains(9)
+
+    actual =tree1.Contains(177)
+    expected =False
+    assert actual == expected
+
+
+
+
+
+
 
--- trees/trees/trees.py
+++ trees/trees/trees.py
@@ -1,92 +1,316 @@
 
-class Binary_Tree:
-    def __init__(self,value):
-        self.value = value
-        self.left = None 
-        self.right = None 
 
-    def add_child (self,value):
-        if value == self.value :
-            return
-        if value < self.value :
-            if self.left :
-                self.left.add_child(value)
-
-            else :
-                self.left = Binary_Tree(value)
-
-        if value > self.value :
-            if self.right :
-                self.right.add_child(value)
-
-            else :
-                self.right = Binary_Tree(value)
-
-    def in_order (self):
-        elements = []
-        #traverse left subtree
-        if self.left:
-            elements+=self.left.in_order()
-
-        # base node 
-        elements.append(self.value)
-
-        if self.right:
-            elements+=self.right.in_order()
-
-        return elements
+class Node ():
+    def __init__(self,value ) :
+        self.value =value
+        self.right=None
+        self.left =None
 
 
-    def pre_order (self):
-        elements = []
+class BinaryTree :
+    def __init__(self):
+        self.root =None
 
-         # base node 
-        elements.append(self.value)
-        #traverse left subtree
-        if self.left:
-            elements+=self.left.in_order()
+    
+    def in_order(self):
+        """
+        Input :None 
+        doing : Traverse the tree in the order left ,root,right
+        Output : print the node's values in  the in_order (left , root ,right)
+        """
+        list1=[]
+        node = self.root
+        def _traverse(node):
+           
+            if node.left:
+                _traverse(node.left)
+
+            list1.append(node.value)
+
+            if node.right:
+                _traverse(node.right)
+
+        _traverse(node)
+        return list1
+
+    def pre_order(self):
+        """
+        Input :None 
+        doing : Traverse the tree in the order root ,left , right  
+        Output : print the node's values in  the in_order ( root ,left ,right)
+        """
+        list2=[]
+        node = self.root
+        def _build(node):
+            list2.append(node.value)
+
+            if node.left:
+                _build(node.left)
+
+            
+
+            if node.right:
+                _build(node.right)
+
+        _build(node)
+        return list2
+    
+    def post_order(self):
+        """
+        Input :None 
+        doing : Traverse the tree in the order left , right , root 
+        Output : print the node's values in  the in_order (left ,right ,root)
+        """
+        list3=[]
+        node = self.root
+        def _build(node):
+            
+
+            if node.left:
+                _build(node.left)
+
+            
+
+            if node.right:
+                _build(node.right)
+
+            list3.append(node.value)
+
+        _build(node)
+        return list3
+
+
+
+
+class BinarySearchTree(BinaryTree):
+    
+        
+    def Add(self, value):
+        """
+        input: value
+        doing: add a value in the correct place 
+        output: the tree wth the new node with that value in the correct location
+        """
+         
+        current = self.root
+        
+        while current:
+
+            if current.right and value > current.value:
+
+                current = current.right 
+
+            elif current.left and value < current.value:
+
+                current = current.left
+                           
+            else:
+                if value < current.value  and current.left is None:
+
+                    current.left = Node(value)
+
+
+                elif  value > current.value  and current.left is None:
+
+                    current.right = Node(value)
+
+                break   
+
+
+        
+
+    def Contains(self , value):
+        
+        """
+        Argument: value
+        Returns: boolean indicating whether or not the value is in the tree at least once.
+        """
+
+        current =self.root
+
+        while current is not None:
+
+            if current.value ==value:
+                return True
+
+            if value < current.value:
+                current=current.left
+
+            if value > current.value:
+                current = current.right
+
+        return False
+
+
+
+            
+      
+
+        
+
+
+
+
+
+if __name__=="__main__":
+    node1=Node(4)
+    node2=Node(2)
+    node3 =Node(8)
+    node4=Node(9)
+    node1.left=node2
+    node1.right=node3
+    node3.right=node4
+
+    
+    tree=BinaryTree()
+    tree.root=node1
+    print(tree.pre_order())
+    # print(tree.pre_order())
+    # print(tree.post_order())
+    tree1 = BinarySearchTree()
+    tree1.root = node1 
+    node1=Node(4)
+    node2=Node(2)
+    node3 =Node(8)
+    node4=Node(9)
+    node2.left=node4
+    node1.left=node2
+    node1.right=node3
+    
+    tree1.Add(9)
+    print(tree1.post_order())
+#    4
+# 2      8     ==> 
+    print(tree1.Contains(9))
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Binary_Tree:
+#     def __init__(self,value):
+#         self.value = value
+#         self.left = None 
+#         self.right = None 
+
+#     def add_child (self,value):
+#         if value == self.value :
+#             return
+#         if value < self.value :
+#             if self.left :
+#                 self.left.add_child(value)
+
+#             else :
+#                 self.left = Binary_Tree(value)
+
+#         if value > self.value :
+#             if self.right :
+#                 self.right.add_child(value)
+
+#             else :
+#                 self.right = Binary_Tree(value)
+
+#     def in_order (self):
+#         elements = []
+#         #traverse left subtree
+#         if self.left:
+#             elements+=self.left.in_order()
+
+#         # base node 
+#         elements.append(self.value)
+
+#         if self.right:
+#             elements+=self.right.in_order()
+
+#         return elements
+
+
+#     def pre_order (self):
+#         elements = []
+
+#          # base node 
+#         elements.append(self.value)
+#         #traverse left subtree
+#         if self.left:
+#             elements+=self.left.in_order()
 
        
 
-        if self.right:
-            elements+=self.right.in_order()
+#         if self.right:
+#             elements+=self.right.in_order()
 
-        return elements
+#         return elements
 
-    def post_order (self):
-        elements = []
+#     def post_order (self):
+#         elements = []
 
          
-        #traverse left subtree
-        if self.left:
-            elements+=self.left.in_order()
+#         #traverse left subtree
+#         if self.left:
+#             elements+=self.left.in_order()
 
        
 
-        if self.right:
-            elements+=self.right.in_order()
+#         if self.right:
+#             elements+=self.right.in_order()
 
-        # base node 
-        elements.append(self.value)
+#         # base node 
+#         elements.append(self.value)
 
-        return elements
+#         return elements
 
 
 
-def build_tree(elements):
-    root =Binary_Tree(elements[0])
-    for i in range(1,len(elements)):
-        root.add_child(elements[i])
+# def build_tree(elements):
+#     root =Binary_Tree(elements[0])
+#     for i in range(1,len(elements)):
+#         root.add_child(elements[i])
 
-    return root 
+#     return root 
 
 
-if __name__ == "__main__":
-    numbers = [2,4,5,1,2,8,345,2,6]
-    numbers_tree = build_tree(numbers)
-    print(numbers_tree.in_order())
-    print(numbers_tree.pre_order())
-    print(numbers_tree.post_order())
+# if __name__ == "__main__":
+#     numbers = [2,4,5,1,2,8,345,2,6]
+#     numbers_tree = build_tree(numbers)
+#     print(numbers_tree.in_order())
+#     print(numbers_tree.pre_order())
+#     print(numbers_tree.post_order())
+#     numbers_tree.add_child(55)
 
 
 
@@ -138,70 +362,70 @@ if __name__ == "__main__":
 
 
 
-# class TreeNode:
-#     def __init__(self,value):
-#         self.value=value
-#         self.children =[]
-#         self.parent = None
+# # class TreeNode:
+# #     def __init__(self,value):
+# #         self.value=value
+# #         self.children =[]
+# #         self.parent = None
 
 
-#     def add_children(self,child):
-#         child.parent =self
-#         self.children.append(child)
+# #     def add_children(self,child):
+# #         child.parent =self
+# #         self.children.append(child)
 
-#     def get_level(self):
-#         level = 0
-#         p = self.parent
-#         while p:
-#             level +=1
-#             p = p.parent
+# #     def get_level(self):
+# #         level = 0
+# #         p = self.parent
+# #         while p:
+# #             level +=1
+# #             p = p.parent
 
-#         return level
+# #         return level
 
 
 
-#     def  __str__(self):
-#         # if self.parent :
+# #     def  __str__(self):
+# #         # if self.parent :
             
-#         #     spaces = " " * self.get_level()
-#         #     print(spaces + "|--"+ self.value)
-#         # else :
-#         #     spaces = " " * self.get_level()
-#         #     print(spaces + self.value)
-#         spaces = " " * self.get_level()*3
-#         prefix = spaces + "|__" if self.parent else ""
-#         print (prefix + self.value)
+# #         #     spaces = " " * self.get_level()
+# #         #     print(spaces + "|--"+ self.value)
+# #         # else :
+# #         #     spaces = " " * self.get_level()
+# #         #     print(spaces + self.value)
+# #         spaces = " " * self.get_level()*3
+# #         prefix = spaces + "|__" if self.parent else ""
+# #         print (prefix + self.value)
         
         
-#         if self.children:
+# #         if self.children:
 
-#             for child in self.children:
-#                 child.__str__()
+# #             for child in self.children:
+# #                 child.__str__()
             
         
 
 
-# def build_tree():
-#     root = TreeNode("Electronics")
+# # def build_tree():
+# #     root = TreeNode("Electronics")
 
-#     laptop = TreeNode("Laptop")
-#     laptop.add_children(TreeNode("Dell1"))
-#     laptop.add_children(TreeNode("Dell2"))
+# #     laptop = TreeNode("Laptop")
+# #     laptop.add_children(TreeNode("Dell1"))
+# #     laptop.add_children(TreeNode("Dell2"))
 
-#     Tv =TreeNode("Tv")
-#     Tv.add_children(TreeNode("Tv1"))
-#     Tv.add_children(TreeNode("Tv2"))
+# #     Tv =TreeNode("Tv")
+# #     Tv.add_children(TreeNode("Tv1"))
+# #     Tv.add_children(TreeNode("Tv2"))
 
-#     root.add_children(laptop)
-#     root.add_children(Tv)
+# #     root.add_children(laptop)
+# #     root.add_children(Tv)
 
-#     return root
+# #     return root
 
 
-# if __name__=="__main__":
+# # if __name__=="__main__":
     
-#     root=build_tree()
-#     root.get_level()
-#     root.__str__()
+# #     root=build_tree()
+# #     root.get_level()
+# #     root.__str__()
 
  
