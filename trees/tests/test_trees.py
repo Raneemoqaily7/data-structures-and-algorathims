e6edbf0ddd4c508f39c5649136cb3891e93b22eb
Raneemoqaily7 <raneemoqaily7@gmail.com>
2022-04-16 10:58:39 +0300

    add tests

--- trees/tests/test_trees.py
+++ trees/tests/test_trees.py
@@ -1,24 +1,51 @@
-from trees.trees import Binary_Tree ,build_tree
+from trees.trees import QNode,Node,BinaryTree
+import pytest
 
-def test_in_order():
-    numbers = [2,4,5,1,2,8,345,2,6]
-    numbers_tree = build_tree(numbers)
-    actual = numbers_tree.in_order()
-    expected = [1, 2, 4, 5, 6, 8, 345]
+def test_breadth_first_one():
+    node1=Node(4)
+    node2=Node(2)
+    node3 =Node(8)
+    node4=Node(9)
+    node5=Node(89)
+    node6=Node(990)
+    node7=Node(29)
+    node1.left=node2
+    node1.right=node3
+    node2.left =node5
+    node2.right=node6
+    node3.right=node4
+    node3.left =node7
+    tree=BinaryTree()
+    tree.root=node1
+    actual =tree.breadth_first()
+    expected =[4, 2, 8, 89, 990, 29, 9]
+    assert actual ==expected
+
+def test_breadth_first_two():
+    node1=Node(4)
+    node2=Node(2)
+    node3 =Node(8)
+    
+    node1.left=node2
+    node1.right=node3
+   
+    tree=BinaryTree()
+    tree.root=node1
+    actual =tree.breadth_first()
+    expected =[4, 2, 8]
+    assert actual ==expected
+
+def test_breadth_first_three():
+    node1=Node(4)
+    tree=BinaryTree()
+    tree.root=node1
+    actual =tree.breadth_first()
+    expected =4
     assert actual == expected
 
+def test_breadth_first_four():
+    tree=BinaryTree()
+    with pytest.raises(Exception):
+        tree.breadth_first("a")
 
-def test_pre_order():
-    numbers = [2,4,5,1,2,8,345,2,6]
-    numbers_tree = build_tree(numbers)
-    actual = numbers_tree.pre_order()
-    expected = [2, 1, 4, 5, 6, 8, 345]
-    assert actual == expected
-
-def test_post_order():
-    numbers = [2,4,5,1,2,8,345,2,6]
-    numbers_tree = build_tree(numbers)
-    actual = numbers_tree.post_order()
-    expected = [1, 4, 5, 6, 8, 345, 2]
-    assert actual == expected
 
