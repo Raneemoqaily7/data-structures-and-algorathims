8bf46c1228a49d465a612b01b7d43d6114f52e5d
Raneemoqaily7 <raneemoqaily7@gmail.com>
2022-05-23 20:39:04 +0300

    Inprocess

--- hashtable/hashtable/hashtable.py
+++ hashtable/hashtable/hashtable.py
@@ -1,10 +1,10 @@
 class HashTable :
-    def __init__(self , size =1024):
+    def __init__(self , size =10):
         self.size =size
         self.map =[None]*size
 
 
-    def get (self , key):
+    def hash (self , key):
         """
         Function takes an  rguments: key and returns Value associated with that key in the table
         """
@@ -12,11 +12,38 @@ class HashTable :
 
         sum_of_ascii =0
         for ch in key :
-            char_ascii =ord(ch)
+            char_ascii = ord(ch)
             sum_of_ascii+=char_ascii
 
-        hashed_key = (sum_of_ascii * 19) % self.size
+        hashed_key = sum_of_ascii   % self.size
+
         return hashed_key
+
+
+    def add (self , k ,value ):
+        hashed_key = self.hash(k)
+        added = self.map[hashed_key] =value
+        return added
+
+
+    def get (self ,k ):
+        hashed_key =self.hash(k)
+        return self.map[hashed_key]
+
+
+    def delete (self , k):
+        hashed_key =self.hash(k)
+        self.map[hashed_key] = None
+
+    def contains(self ,k):
+        hashed_key = self.hash(k)
+        for i in range (self.size):
+            if i == hashed_key :
+                return True
+            else :
+                return False
+
+
 
 
 
@@ -24,4 +51,19 @@ class HashTable :
 
 if __name__=="__main__":
     hashtable =HashTable()
-    print(hashtable.get("cloud"))
+    print(hashtable.hash("march 6"))
+    
+    # hashtable.add("aCt" , "Jessica")
+    hashtable.add("Owner" ,"John")
+    hashtable.add("Dog" , "Jessi")
+    hashtable.add("Mobile" ,"+9624567469")
+    
+    print(hashtable.get("Dog"))
+    print(hashtable.get("Owner"))
+    print (hashtable.map)
+    hashtable.delete("Dog")
+    print(hashtable.contains("Owner"))
+    print (hashtable.map)
+
+
+
