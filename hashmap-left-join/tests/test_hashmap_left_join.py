8339fa65d9ccfa30aef375096aad5df9bec4ae0f
Raneemoqaily7 <raneemoqaily7@gmail.com>
2022-06-06 18:41:31 +0300

    add readme

--- hashmap-left-join/tests/test_hashmap_left_join.py
+++ hashmap-left-join/tests/test_hashmap_left_join.py
@@ -1,5 +1,90 @@
-from hashmap_left_join import __version__
+import pytest
+from hashmap_left_join.hashmap import Hashmap, left_join
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_left_join_empty():
+    hash1 = Hashmap()
+    hash2= Hashmap()
+
+    hash1.add('name', 'raneem')
+
+    hash3 = left_join(hash1, hash2)
+    assert hash3.get('name') == ['raneem', 'Null']
+
+
+
+
+
+
+    
+    
+
+
+
+
+
+
+
+def test_two_collsion():
+    hash1 = Hashmap()
+    hash2 = Hashmap()
+
+    hash1.add('fond', 'enamored')
+
+
+
+    hash1.add('ofnd','test')
+
+    hash2.add('fond', 'averse')
+    hash2.add('guide', 'follow')
+
+    hash3 = left_join(hash1, hash2)
+
+
+    assert hash3.get('fond') == ['enamored', 'averse']
+    assert hash3.get('ofnd') == ['test', 'Null']
+
+
+
+def test_oneft_join():
+
+
+    hash1 = Hashmap()
+    hash2 = Hashmap()
+    hash1.add('fond', 'enamored')
+    hash1.add('wrath', 'anger')
+
+    hash1.add('diligent', 'employed')
+
+    hash1.add('outfit', 'garb')
+    
+
+    
+    hash2.add('fond', 'averse')
+    hash2.add('wrath', 'delight')
+    hash2.add('diligent', 'idle') 
+   
+
+    hash3 = left_join(hash1, hash2)
+
+    assert hash3.get('outfit') == ['garb', 'Null']
+    assert hash3.get('fond') == ['enamored', 'averse']
+
+    assert hash3.get('wrath') == ['anger', 'delight']
+
+
+
+    assert hash3.get('diligent') == ['employed', 'idle']
+
+
+
+
+
+
+
+
+
+
+
+
+
