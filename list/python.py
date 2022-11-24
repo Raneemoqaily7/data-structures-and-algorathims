42db90d597fab46882aa923ed59db1b101ccdd53
Raneemoqaily7 <raneemoqaily7@gmail.com>
2022-11-24 10:38:09 +0000

    oop

--- list/python.py
+++ list/python.py
@@ -53,16 +53,81 @@
 
 
 
-def decotaror (func):
-    print ("iam decorator") 
-    return func ()
+# def decotaror (func):
+#     print ("iam decorator") 
+#     return func ()
     
 
 
-@decotaror
-def say_hello():
-    p="hello world"
-    return p
+# @decotaror
+# def say_hello():
+#     p="hello world"
+#     return p
 
-print (say_hello)
+# print (say_hello)
+
+
+
+# list = [i for i in range (2 , 11) if i %2 == 0]
+# print (list)
+
+
+
+# matrix = [ [j for j in range  (3)] for  _ in range (6)]
+# print (matrix)
+
+# list = [i  for  i in "hello world"]
+# print (list)
+
+
+# list = ["even number" if i %2 == 0 else "odd number" for i in range (11)]
+# print (list)
+
+
+
+# list = [i for i in range (11) if i %2 == 0 if i%2 !=0]
+# print (list)
+
+# Reverse each string in tuple
+# List = [string[::-1] for string in ["geeks" ,"for"]]
+
+# # Display list
+# print(list)
+
+
+# def sum (i):
+    
+#         sum = 0
+#         for j in str (i) :
+#             sum += int (j)
+#         return sum 
+
+# list = [231 ,345,53]
+
+# list1 = [sum(j) for j in list ]
+# print (list1)
+        
+
+            
+class Car :
+    def __init__(self , name , price , model):
+        self.name = name 
+        self.price = price
+        self.model = model
+
+    def difintion (self):
+        if self.model > 2019 :
+            self.price = self.price + 20
+        else :
+                self.price = self.price -20
+        return self.price
+
+Bmw = Car ("Bmw" , 29 , 1999)
+# print (Bmw.price  )
+
+print (Bmw.difintion())
+print (Bmw.price)
+
+
+
 
